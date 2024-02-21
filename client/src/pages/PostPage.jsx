eb3e847366dabfde705c6c7c86eb061f10bb3e2f
Leki Tshering <lekitshering@academy.bt>
2024-02-21 23:54:52 +0600

    Add to action

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Button, Spinner } from "flowbite-react";
 import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -67,6 +68,9 @@ export default function PostPage() {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+      <div className="max-w-4xl max-auto w-full">
+        <CallToAction />
+      </div>
     </main>
   );
 }
