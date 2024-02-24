e107ab4e71d55780a041e50521f1e00f6246217c
Leki Tshering <lekitshering@academy.bt>
2024-02-24 12:35:23 +0600

    changes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
@@ -7,6 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -28,9 +28,6 @@ app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello fr om the mern app");
-});
 // routing the routes in main app
 
 app.use("/api/user", userRoutes);
@@ -39,6 +36,7 @@ app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
