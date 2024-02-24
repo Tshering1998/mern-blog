3abbae32a71b62a248faecdb69251c21183c5807
Leki Tshering <lekitshering@academy.bt>
2024-02-24 11:24:16 +0600

    about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,48 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Sahand Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flx-col gap-6">
+            <p>
+              Welcome to Sahand Blog At Sahand Blog, we believe in the power of
+              knowledge and the beauty of sharing ideas. Our journey began with
+              a passion for creating a space where individuals could delve into
+              insightful content, explore diverse perspectives, and connect with
+              a community driven by curiosity. Whether you're a seasoned
+              enthusiast or just starting your exploration, Sahand Blog is here
+              to be your guide through a world of engaging and thought-provoking
+              topics. Our Vision and Mission Sahand Blog is more than just a
+              platform; it's a vision brought to life. Our mission is to foster
+              a vibrant online community that celebrates curiosity, fosters
+              intellectual growth, and embraces the joy of continuous learning.
+              We curate content that spans a wide array of subjects, from
+              science and technology to arts and culture, with the goal of
+              providing a rich tapestry of insights that inspire and educate.
+              Join us on this journey of discovery, where the pursuit of
+              knowledge is at the heart of everything we do. **Meet the Team
+              Behind Sahand Blog
+              <p>
+                Behind every article, infographic, and discussion on Sahand Blog
+                is a dedicated team of writers, researchers, and creators. We
+                come from diverse backgrounds, united by a shared commitment to
+                excellence and a genuine love for learning. Our contributors
+                bring a wealth of experience and expertise to the table,
+                ensuring that each piece of content is not only informative but
+                also crafted with passion. Get to know the faces behind Sahand
+                Blog as we continue to grow, evolve, and explore the boundless
+                realms of knowledge together. --- Feel free to customize these
+                paragraphs to better fit the specific tone, style, and details
+                you'd like to highlight on Sahand Blog's About
+              </p>
+              page.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
